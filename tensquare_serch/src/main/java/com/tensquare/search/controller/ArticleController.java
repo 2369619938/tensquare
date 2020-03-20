d23308261e697daec2be7b9f95c9dd701f4b457d
admin <2369619938@qq.com>
2020-03-20 17:21:56 +0800

    使用ElasticSearch实现文章搜索功能

---
+++ tensquare_serch/src/main/java/com/tensquare/search/controller/ArticleController.java
@@ -0,0 +1,31 @@
+package com.tensquare.search.controller;
+
+import com.tensquare.search.pojo.Article;
+import com.tensquare.search.service.ArticleService;
+import entity.PageResult;
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/article")
+@CrossOrigin
+public class ArticleController {
+    @Autowired
+    private ArticleService articleService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Result save(@RequestBody Article article){
+        articleService.save(article);
+        return new Result(true, StatusCode.OK,"添加成功");
+    }
+
+    @RequestMapping(value = "/{key}/{page}/{size}",method = RequestMethod.GET)
+    public Result findByKey(@PathVariable String key,@PathVariable int page,@PathVariable int size){
+        Page<Article> pageDate = articleService.findByKey(key,page,size);
+        return new Result(true, StatusCode.OK,"添加成功",new PageResult<Article>(pageDate.getTotalElements(),pageDate.getContent()));
+    }
+}
---
+++ tensquare_serch/src/main/java/com/tensquare/search/pojo/Article.java
@@ -0,0 +1,54 @@
+package com.tensquare.search.pojo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+
+import java.io.Serializable;
+
+@Document(indexName = "tensquare_article",type = "article")
+public class Article implements Serializable {
+    @Id
+    private String id;
+
+    //是否索引 看該域能被搜索
+    //是否分詞 是看整体匹配还是单词匹配
+    //是否存储 是否在页面上显示
+    @Field(index = true,analyzer = "ik_max_word",searchAnalyzer ="ik_max_word" )
+    private String title;
+    @Field(index = true,analyzer = "ik_max_word",searchAnalyzer ="ik_max_word" )
+    private String content;
+    private String state;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
